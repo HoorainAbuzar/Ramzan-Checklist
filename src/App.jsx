cba953d750cb354d4cfe2836afb3a169195d6a33
Abuzar Sheikh <abuzarssheikh3@gmail.com>
2025-03-13 10:45:38 +0530

    added dua

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 // import MainPage from './components/MainPage.jsx'
 import Checklist from "./components/CheckList.jsx";
+import Dua from "./components/Dua.jsx";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Checklist />
+      <Routes>
+        <Route path={"/"} element={<Dua />} />
+        <Route path={"/checklist"} element={<Checklist />} />
+      </Routes>
     </div>
   );
 }
