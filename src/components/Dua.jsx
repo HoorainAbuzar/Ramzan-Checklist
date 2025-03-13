c2f1f887f650a27989f1b0a2e191f2fb84bbc328
Abuzar Sheikh <abuzarssheikh3@gmail.com>
2025-03-13 10:51:12 +0530

    done

--- src/components/Dua.jsx
+++ src/components/Dua.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom'
 const Dua = () => {
   const navigate = useNavigate()
   return (
-    <div className='w-full md:w-[40%] h-screen p-4 bg-gradient-to-b from-green-200 to-green-600 flex flex-col items-center justify-center'>
-      <img className='shadow-xl' src={img} alt="" />
+    <div className='w-full h-screen p-8 bg-gradient-to-b from-green-200 to-green-600 flex flex-col items-center justify-center'>
+      <img className='shadow-xl md:w-[40%]' src={img} alt="" />
       <button onClick={()=> navigate('/checklist')} className='bg-blue-600 hover:bg-blue-700 transition-all duration-300 w-full rounded py-2 mt-3 text-white'>Next</button>
     </div>
   )
