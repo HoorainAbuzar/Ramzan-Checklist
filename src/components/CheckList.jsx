c2730d8350681021f757f88aa3adc88088d6b2d1
Abuzar Sheikh <abuzarssheikh3@gmail.com>
2025-03-30 21:18:04 +0530

    lan

--- src/components/CheckList.jsx
+++ src/components/CheckList.jsx
@@ -135,7 +135,7 @@ const Checklist = () => {
           {isEnglish ? "Download your result" : "نتائج ڈاؤن لوڈ کریں"}
         </button>
         <button
-          onClick={toggleLanguage}
+          // onClick={toggleLanguage}
           className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
         >
           {isEnglish ? "Change Language" : "زبان بدلیں"}
